5a1b6cae3743494ef99cdbaa97dd362e21cc9632
gratatataa <extrobin2121@gmail.com>
2022-11-29 16:07:51 +0900

    11-29

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 (() => {
-  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
-  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
-  let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
-  let enterNewScene = false; // 새로운 scene이 시작된 순간 true
-  let acc = 0.2;
-  let delayedYOffset = 0;
-  let rafId;
-  let rafState;
-
   const sceneInfo = [
+    {
+      // 0
+      type: "sticky",
+      heightNum: 5, //브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
     {
       // 1
       type: "normal",
-      // heightNum: 5, // type normal에서는 필요 없음
+      heightNum: 5,
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-1"),
-        content: document.querySelector("#scroll-section-1 .description"),
       },
     },
     {
@@ -26,38 +25,6 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-2"),
-        messageA: document.querySelector("#scroll-section-2 .a"),
-        messageB: document.querySelector("#scroll-section-2 .b"),
-        messageC: document.querySelector("#scroll-section-2 .c"),
-        pinB: document.querySelector("#scroll-section-2 .b .pin"),
-        pinC: document.querySelector("#scroll-section-2 .c .pin"),
-        canvas: document.querySelector("#video-canvas-1"),
-        context: document.querySelector("#video-canvas-1").getContext("2d"),
-        videoImages: [],
-      },
-      values: {
-        videoImageCount: 960,
-        imageSequence: [0, 959],
-        canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
-        canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
-        messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
-        messageB_translateY_in: [30, 0, { start: 0.5, end: 0.55 }],
-        messageC_translateY_in: [30, 0, { start: 0.72, end: 0.77 }],
-        messageA_opacity_in: [0, 1, { start: 0.15, end: 0.2 }],
-        messageB_opacity_in: [0, 1, { start: 0.5, end: 0.55 }],
-        messageC_opacity_in: [0, 1, { start: 0.72, end: 0.77 }],
-        messageA_translateY_out: [0, -20, { start: 0.3, end: 0.35 }],
-        messageB_translateY_out: [0, -20, { start: 0.58, end: 0.63 }],
-        messageC_translateY_out: [0, -20, { start: 0.85, end: 0.9 }],
-        messageA_opacity_out: [1, 0, { start: 0.3, end: 0.35 }],
-        messageB_opacity_out: [1, 0, { start: 0.58, end: 0.63 }],
-        messageC_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
-        pinB_scaleY: [0.5, 1, { start: 0.5, end: 0.55 }],
-        pinC_scaleY: [0.5, 1, { start: 0.72, end: 0.77 }],
-        pinB_opacity_in: [0, 1, { start: 0.5, end: 0.55 }],
-        pinC_opacity_in: [0, 1, { start: 0.72, end: 0.77 }],
-        pinB_opacity_out: [1, 0, { start: 0.58, end: 0.63 }],
-        pinC_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
       },
     },
     {
@@ -67,140 +34,20 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-3"),
-        canvasCaption: document.querySelector(".canvas-caption"),
-        canvas: document.querySelector(".image-blend-canvas"),
-        context: document.querySelector(".image-blend-canvas").getContext("2d"),
-        imagesPath: [
-          "./images/blend-image-1.jpg",
-          "./images/blend-image-2.jpg",
-        ],
-        images: [],
-      },
-      values: {
-        rect1X: [0, 0, { start: 0, end: 0 }],
-        rect2X: [0, 0, { start: 0, end: 0 }],
-        blendHeight: [0, 0, { start: 0, end: 0 }],
-        canvas_scale: [0, 0, { start: 0, end: 0 }],
-        canvasCaption_opacity: [0, 1, { start: 0, end: 0 }],
-        canvasCaption_translateY: [20, 0, { start: 0, end: 0 }],
-        rectStartY: 0,
       },
     },
   ];
 
-  function setCanvasImages() {
-    let imgElem;
-    for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
-      imgElem = new Image();
-      imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
-      sceneInfo[0].objs.videoImages.push(imgElem);
-    }
-
-    let imgElem2;
-    for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
-      imgElem2 = new Image();
-      imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
-      sceneInfo[2].objs.videoImages.push(imgElem2);
-    }
-
-    let imgElem3;
-    for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
-      imgElem3 = new Image();
-      imgElem3.src = sceneInfo[3].objs.imagesPath[i];
-      sceneInfo[3].objs.images.push(imgElem3);
-    }
-  }
-
-  function checkMenu() {
-    if (yOffset > 44) {
-      document.body.classList.add("local-nav-sticky");
-    } else {
-      document.body.classList.remove("local-nav-sticky");
-    }
-  }
-
   function setLayout() {
-    // 각 스크롤 섹션의 높이 세팅
+    //각 스크롤 높이 셋팅
     for (let i = 0; i < sceneInfo.length; i++) {
-      if (sceneInfo[i].type === "sticky") {
-        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      } else if (sceneInfo[i].type === "normal") {
-        sceneInfo[i].scrollHeight =
-          sceneInfo[i].objs.content.offsetHeight + window.innerHeight * 0.5;
-      }
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
 
-    yOffset = window.pageYOffset;
-
-    let totalScrollHeight = 0;
-    for (let i = 0; i < sceneInfo.length; i++) {
-      totalScrollHeight += sceneInfo[i].scrollHeight;
-      if (totalScrollHeight >= yOffset) {
-        currentScene = i;
-        break;
-      }
-    }
-    document.body.setAttribute("id", `show-scene-${currentScene}`);
-
-    const heightRatio = window.innerHeight / 1080;
-    sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
-    sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+    console.log(sceneInfo);
   }
-
-  window.addEventListener("load", () => {
-    setLayout(); // 중간에 새로고침 시, 콘텐츠 양에 따라 높이 계산에 오차가 발생하는 경우를 방지하기 위해 before-load 클래스 제거 전에도 확실하게 높이를 세팅하도록 한번 더 실행
-    document.body.classList.remove("before-load");
-    setLayout();
-    sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
-
-    // 중간에서 새로고침 했을 경우 자동 스크롤로 제대로 그려주기
-    let tempYOffset = yOffset;
-    let tempScrollCount = 0;
-    if (tempYOffset > 0) {
-      let siId = setInterval(() => {
-        scrollTo(0, tempYOffset);
-        tempYOffset += 5;
-
-        if (tempScrollCount > 20) {
-          clearInterval(siId);
-        }
-        tempScrollCount++;
-      }, 20);
-    }
-
-    window.addEventListener("scroll", () => {
-      yOffset = window.pageYOffset;
-      scrollLoop();
-      checkMenu();
-
-      if (!rafState) {
-        rafId = requestAnimationFrame(loop);
-        rafState = true;
-      }
-    });
-
-    window.addEventListener("resize", () => {
-      if (window.innerWidth > 900) {
-        window.location.reload();
-      }
-    });
-
-    window.addEventListener("orientationchange", () => {
-      scrollTo(0, 0);
-      setTimeout(() => {
-        window.location.reload();
-      }, 500);
-    });
-
-    document
-      .querySelector(".loading")
-      .addEventListener("transitionend", (e) => {
-        document.body.removeChild(e.currentTarget);
-      });
-  });
-
-  setCanvasImages();
+  setLayout();
 })();
